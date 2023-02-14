35b659b4f318e87b9241a29f0817955956ac80e4
Alex Druzina <52024657+saashd@users.noreply.github.com>
2023-02-14 14:15:11 +0200

    Update order.js
    add api for monthly income

--- server/routes/order.js
+++ server/routes/order.js
@@ -66,5 +66,41 @@ router.get("/", verifyTokenAndAdmin, async (req, res) => {
   }
 });
 
+// GET MONTHLY INCOME
+
+router.get("/income", verifyTokenAndAdmin, async (req, res) => {
+  const productId = req.query.pid;
+  const date = new Date();
+  const previousMonth = new Date(date.setMonth(date.getMonth() - 2));
+
+
+  try {
+    const income = await Order.aggregate([
+      {
+        $match: {
+          createdAt: { $gte: previousMonth },
+          ...(productId && {
+            products: { $elemMatch: { productId } },
+          }),
+        },
+      },
+      {
+        $project: {
+          month: { $month: "$createdAt" },
+          sales: "$amount",
+        },
+      },
+      {
+        $group: {
+          _id: "$month",
+          total: { $sum: "$sales" },
+        },
+      },
+    ]);
+    res.status(200).json(income);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
